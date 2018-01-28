a2185f96be75bfe574ee63df33075dcf5b14ce6d
NoahZook <zook.noah@gmail.com>
2018-01-28 11:08:46 -0600

    buttons work, trying to populate authors in authorList's fields

--- SoftwareEnterpriseS1/src/assignment1/controller.java
+++ SoftwareEnterpriseS1/src/assignment1/controller.java
@@ -1,15 +1,35 @@
 package assignment1;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
-public class controller {
-
+public class controller implements Initializable{
+	
+	@FXML
+    private ListView<String> listView;
+	
+	public void initialize(URL url, ResourceBundle rb) {
+		List<String> list = new ArrayList<String>();
+		list.add("Item A");
+        list.add("Item B");
+        list.add("Item C");
+		//listView.setItems(FXCollections.observableList(list));
+	}
+	
 	public void changeToMain(ActionEvent event) throws IOException {
         Parent tableViewParent = FXMLLoader.load(getClass().getResource("main.fxml"));
         Scene tableViewScene = new Scene(tableViewParent);
@@ -30,17 +50,18 @@ public class controller {
         window.show();
     }
     
-    public void changeToDetail(ActionEvent event) throws IOException {
-        Parent tableViewParent = FXMLLoader.load(getClass().getResource("AuthorDetailView.fxml"));
-        Scene tableViewScene = new Scene(tableViewParent);
-
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        
-        window.setScene(tableViewScene);
-        window.show();
-    }
-    
     public void exit(ActionEvent event) throws IOException {
         System.exit(0);
     }
+        
 }
+
+/*
+<FXCollections fx:factory="observableArrayList">
+<String fx:value="Ernest Hemingway"/>
+<String fx:value="Mark Twain"/>
+<String fx:value="Stephen King"/>
+<String fx:value="George Orwell"/>
+<String fx:value="Ray Bradbury"/>
+</FXCollections>
+*/
