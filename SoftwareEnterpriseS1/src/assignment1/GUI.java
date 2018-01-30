165c049a78c27eda345f094bdf4fee4ddeac3c44
jtrejo2 <jontrejo.jt@gmail.com>
2018-01-29 20:45:10 -0600

    Implement log4j in to project

--- SoftwareEnterpriseS1/src/assignment1/GUI.java
+++ SoftwareEnterpriseS1/src/assignment1/GUI.java
@@ -6,9 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class GUI extends Application {
      //small change to launch menu.fxml
 	//second sample comment
+	private static Logger logger = LogManager.getLogger(GUI.class);
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
@@ -20,6 +24,11 @@ public class GUI extends Application {
     }
 
     public static void main(String[] args) {
+    	//won't show because of the default logger config
+  		logger.debug("This is a debug log message23");
+  		
+  		//but this will
+  		logger.error("This is an error log message24");
         launch(args);
     }
     
