40ba2d2681aa2b9fafe9e30f9072f0b4d3dc1f1e
NoahZook <zook.noah@gmail.com>
2018-01-30 00:06:06 -0600

    this is about as much as you can do with detail controller until author is implemented

--- SoftwareEnterpriseS1/src/assignment1/DetailController.java
+++ SoftwareEnterpriseS1/src/assignment1/DetailController.java
@@ -1,34 +1,34 @@
 package assignment1;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import java.awt.event.ActionEvent;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class DetailController {
+	private static Logger logger = LogManager.getLogger();
+	private static menuController menuController;
 
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
-    @FXML
-    private Button AuthorList;
-
-    @FXML
-    private Button Save;
-
-    @FXML
-    void changeToList(ActionEvent event) {
-
-    }
-
+    @FXML private Button Save;
+    
+    public DetailController(Author author) {
+		menuController = menuController.getInstanceofmenuContoller();
+		//this.author = author;
+	}
+    
+    @FXML void saveAuthorDetail(ActionEvent event) {
+		logger.info("calling saveAuthorDetail()");
+		
+		logger.info("author saved");
+		
+		//menuController.loadAList();
+	}
+    
     @FXML
     void initialize() {
-        assert AuthorList != null : "fx:id=\"AuthorList\" was not injected: check your FXML file 'AuthorDetailView.fxml'.";
-        assert Save != null : "fx:id=\"Save\" was not injected: check your FXML file 'AuthorDetailView.fxml'.";
-
+    	
     }
 }
